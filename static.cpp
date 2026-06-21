9f495290e3a504a4569f465be1759f0f31c35ac1
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-21 18:15:33 -0500

    checks times called
    displays how many times the static variables is called

--- static.cpp
+++ static.cpp
@@ -8,10 +8,18 @@ void f(){
     cout << x <<endl;
 }
 
+int count(int x){
+    static int time_called = 0;
+    time_called++;
+    cout << "count has been called " << time_called << " times "<<endl;
+    if(x == 0){
+        return 0;
+
+    }
+    return 1+ count(x - 1);
+}
+
 int main(){
-    f();
-    f();
-    f();
-    f();
+    cout << count(10) << endl;
     return 0;
 }
